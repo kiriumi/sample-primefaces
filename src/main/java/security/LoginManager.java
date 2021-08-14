68c96bbf15c3c7734be829920f10316049de16d3
kiriumi <kiriumi.miko@gmail.com>
2021-08-14 23:28:53 +0900

    自動ログイン機能を追加

--- src/main/java/security/LoginManager.java
+++ src/main/java/security/LoginManager.java
@@ -1,6 +1,8 @@
 package security;
 
 import java.io.Serializable;
+import java.util.Map;
+import java.util.ResourceBundle;
 
 import javax.enterprise.context.SessionScoped;
 import javax.faces.context.ExternalContext;
@@ -15,7 +17,11 @@ import org.apache.commons.lang3.StringUtils;
 @SessionScoped
 public class LoginManager implements Serializable {
 
-    public static final String SESSION_KEY_USER_ID = "security.LoginManager.userId";
+    private static final String SESSION_KEY_USER_ID = "security.LoginManager.userId";
+
+    private static final String SESSION_COOKIE = "JSESSIONID";
+
+    private static final int UNIT_DAY = 60 * 60 * 24;
 
     @Inject
     private ExternalContext extCtx;
@@ -25,23 +31,55 @@ public class LoginManager implements Serializable {
     }
 
     private boolean logined;
+    private boolean autoLogin;
 
-    public void login(String userId) {
+    public void login(String userId, boolean autoLogin) {
 
         if (logined) {
             return;
         }
 
+        // ユーザIDをセッションから取得できるよう設定
         extCtx.getSessionMap().put(SESSION_KEY_USER_ID, StringUtils.defaultString(userId, ""));
+
+        // セッションIDの変更
         HttpServletRequest req = (HttpServletRequest) extCtx.getRequest();
         req.changeSessionId();
+
         this.logined = true;
+
+        if (autoLogin) {
+            // 自動ログインの場合
+            this.autoLogin = true;
+
+            // 有効期限の取得
+            ResourceBundle bundle = ResourceBundle.getBundle("ApplicationConfig");
+            String strAutoLoginInterval = bundle.getString("autologin.interval.day");
+            int autoLoginInterval = Integer.parseInt(strAutoLoginInterval) * UNIT_DAY;
+
+            extCtx.setSessionMaxInactiveInterval(autoLoginInterval); // セッションの有効期限を設定
+
+            // セッションクッキーの有効期限を設定
+            Map<String, Object> cookieAttr = Map.of("path", extCtx.getRequestContextPath(), "maxAge", autoLoginInterval, "httpOnly", true);
+            extCtx.addResponseCookie(SESSION_COOKIE, extCtx.getSessionId(false), cookieAttr);
+        }
+
     }
 
-    public boolean logout() {
+    public void logout() {
+
+        if (autoLogin) {
+            // セッションクッキーを破棄
+            Map<String, Object> cookieAttr = Map.of("path", extCtx.getRequestContextPath(), "maxAge", 0, "httpOnly", true);
+            extCtx.addResponseCookie(SESSION_COOKIE, extCtx.getSessionId(false), cookieAttr);
+
+            this.autoLogin = false;
+        }
+
         extCtx.getSessionMap().remove(SESSION_KEY_USER_ID);
         extCtx.invalidateSession();
-        return logined = false;
+
+        logined = false;
     }
 
     public boolean logined() {
