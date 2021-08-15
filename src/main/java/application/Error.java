88d88607c8edbbbb8a063163de1dac98c7e692ee
kiriumi <kiriumi.miko@gmail.com>
2021-08-15 13:34:14 +0900

    エラー画面を改修

--- src/main/java/application/Error.java
+++ src/main/java/application/Error.java
@@ -1,10 +1,11 @@
 package application;
 
+import java.util.ResourceBundle;
+
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 import faces.BaseBackingBean;
-
 
 /**
  * システム障害画面
@@ -16,6 +17,11 @@ import faces.BaseBackingBean;
 public class Error extends BaseBackingBean {
 
     public String init() {
+
+        if (messageService().isEmpty()) {
+            ResourceBundle bundle = ResourceBundle.getBundle("ApplicationConfig");
+            return redirect(bundle.getString("login.page"));
+        }
         return null;
     }
 
