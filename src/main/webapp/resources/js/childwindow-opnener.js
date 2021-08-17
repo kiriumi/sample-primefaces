04b27a2e5a1eb2e20452bcc7b6924ee829250f62
kiriumi <kiriumi.miko@gmail.com>
2021-08-17 11:48:24 +0900

    JavaScriptを整理

--- src/main/webapp/resources/js/childwindow-opnener.js
+++ src/main/webapp/resources/js/childwindow-opnener.js
@@ -3,8 +3,18 @@ function openChildWindow(url, windowName, windowFeature) {
     var parentToken = document.getElementById("token").value;
     var namespace = Math.random().toString(32).substring(2);
 
-    var childWindow =  window.open(url + '?token=' + parentToken　+  '&childTokenNamespace=' + namespace ,windowName , windowFeature);
+    var childWindow = window.open(url + '?token=' + parentToken + '&childTokenNamespace=' + namespace, windowName, windowFeature);
     childWindow.focus();
 
     return childWindow;
+}
+
+/**
+ * 子画面を閉じた後、親画面の要素をクリックしてアクションを実行する
+ * ※例：<p: commandButton ajax="true" value="閉じる" action="#{sample.close()}" onsuccess="closeChildWindowWithAction('action')" />
+ */
+function closeChildWindow(elementId) {
+    window.opener.document.getElementById(elementId).click();
+    window.close();
+    window.opener.focus();
 }
--- src/main/webapp/resources/js/window-controller.js
+++ src/main/webapp/resources/js/window-controller.js
@@ -5,7 +5,7 @@ document.addEventListener("keydown", function(event) {
 
     var key = event.key;
 
-    if (key !== 13) return;
+    if (key !== "Enter") return;
 
     var elem = event.target;
 
@@ -45,3 +45,87 @@ document.addEventListener("contextmenu", function(event) {
 
 }, false)
 */
+
+/**
+ * ページを離れる際に、入力項目に変更があったら確認する
+ * 使用方法
+ * ・form要素のclass属性に「confirm-onunload」を指定する
+ * ・確認が不要な処理を行う要素のclass属性に「not-confirm」を指定する
+ */
+$(function() {
+
+    // フラグの初期化
+    var confirm = false;
+    var changed = false;
+    var notConfirm = false;
+
+    if ($(".confirm-beforeunload").length != 0) confirm = true;
+
+    // ページの移動・更新を検知した時の処理
+    $(window).on('beforeunload', function() {
+        if (confirm && changed && !notConfirm) {
+            return 'このページを離れます。\nよろしいですか？';
+        }
+        submit = false;
+    });
+
+    // 文字入力項目に変更があった時の処理
+    // ※changeだと、文字入力が未確定でも変更されたと判断するため、keydownとkeyupにする
+    $('input, textarea').on('keydown keyup', function() {
+        changed = true;
+    });
+
+    // その他入力項目に変更があった時の処理
+    $('input, select').change(function() {
+        changed = true;
+    });
+
+    // 送信項目クリック時の処理
+    $(".not-confirm").on('click', function() {
+        notConfirm = true;
+    });
+
+});
+
+function showBlockUi() {
+    PrimeFaces.widgets['bui'].show();
+}
+
+function hideBlockUi() {
+    PrimeFaces.widgets['bui'].hide();
+}
+
+document.addEventListener("submit", function() {
+    $('form').css('pointer-events', 'none');
+    PrimeFaces.widgets['bui'].show();
+});
+
+/**
+ * PrimeFacesの日本語カレンダー
+ */
+PrimeFaces.locales['ja'] = {
+    closeText: '閉じる',
+    prevText: '前月',
+    nextText: '翌月',
+    monthNames: ['１月', '２月', '３月', '４月', '５月', '６月', '７月', '８月', '９月', '１０月', '１１月', '１２月'],
+    monthNamesShort: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+    dayNames: ['日曜', '月曜', '火曜', '水曜', '木曜', '金曜', '土曜'],
+    dayNamesShort: ['日', '月', '火', '水', '木', '金', '土'],
+    dayNamesMin: ['日', '月', '火', '水', '木', '金', '土'],
+    weekHeader: '週',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: true,
+    yearSuffix: '年',
+    timeOnlyTitle: '時間のみ',
+    timeText: '時間',
+    hourText: '時',
+    minuteText: '分',
+    secondText: '秒',
+    currentText: '今日',
+    ampm: false,
+    month: '月',
+    week: '週',
+    day: '日',
+    allDayText: '全日'
+};
