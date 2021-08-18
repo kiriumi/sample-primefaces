ac9984282e0ce9115807742fc0df5189986af576
kiriumi <kiriumi.miko@gmail.com>
2021-08-19 01:50:44 +0900

    改行の箇所の修正

--- src/main/java/application/ChildWindow.java
+++ src/main/java/application/ChildWindow.java
@@ -13,21 +13,22 @@ import token.TokenCheck;
 @TokenCheck
 public class ChildWindow extends BaseBackingBean {
 
-    @Getter
-    @Setter
-    private String passInfo;
+	@Getter
+	@Setter
+	private String passInfo;
 
-    public String init() {
-        this.passInfo = (String) session().get("application.Top.passInfo");
-        return null;
-    }
+	public String init() {
+		this.passInfo = (String) session().get("application.Top.passInfo");
+		return null;
+	}
 
-    public String close() {
-        session().put("application.Top.passInfo", "親画面に渡した情報");
-        return null;
-    }
+	public String close() {
+		session().put("application.Top.passInfo", "親画面に渡した情報");
+		return null;
+	}
 
-    public String goChildWindow2() {
-        return redirect("/application/childWindow2.xhtml");
-    }
+	public String goChildWindow2() {
+		return redirect("/application/childWindow2.xhtml");
+	}
+
 }
