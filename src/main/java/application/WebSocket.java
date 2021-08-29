ff76db6cf77a3f120ee695dacfac7b68688a5990
kiriumi <kiriumi.miko@gmail.com>
2021-08-29 12:18:01 +0900

    指定のグループのセッションのみWebSocketで送信するサンプルに改修

--- src/main/java/application/WebSocket.java
+++ src/main/java/application/WebSocket.java
@@ -3,15 +3,18 @@ package application;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
-@ServerEndpoint("/websocket")
+@ServerEndpoint("/websocket/{group}")
 public class WebSocket {
 
     //    @OnMessage
@@ -19,23 +22,44 @@ public class WebSocket {
     //        return message;
     //    }
 
-    private static Set<Session> sessionSet = Collections.synchronizedSet(new HashSet<Session>());
+    private static Map<String, Set<Session>> sessionMap = new ConcurrentHashMap<>();
 
     @OnOpen
-    public void onOpen(Session session) {
+    public void onOpen(Session session, @PathParam("group") String group) {
+
+        Set<Session> sessionSet = sessionMap.get(group);
+        if (sessionSet == null) {
+            sessionSet = Collections.synchronizedSet(new HashSet<Session>());
+        }
         sessionSet.add(session);
+        sessionMap.put(group, sessionSet);
     }
 
     @OnMessage
-    public void broadcastOnMessage(String message) throws IOException {
+    public void broadcastOnMessage(String message, Session session, @PathParam("group") String group)
+            throws IOException {
 
-        for (Session session : sessionSet) {
-            session.getBasicRemote().sendText(message);
+        String option = session.getRequestParameterMap().get("option").get(0);
+        System.out.println(option);
+
+        Set<Session> sessionSet = sessionMap.get(group);
+        for (Session eachSession : sessionSet) {
+            eachSession.getBasicRemote().sendText(message);
         }
     }
 
     @OnClose
-    public void onClose(Session session) {
+    public void onClose(Session session, @PathParam("group") String group) {
+
+        Set<Session> sessionSet = sessionMap.get(group);
+        if (sessionSet == null) {
+            return;
+        }
+
         sessionSet.remove(session);
+
+        if (sessionSet.isEmpty()) {
+            sessionMap.remove(group);
+        }
     }
 }
